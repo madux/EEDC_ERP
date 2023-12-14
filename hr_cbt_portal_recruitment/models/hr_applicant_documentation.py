683c0d364283a27cdbaae7c3bf8f3fcfeda39d1f
Paul Ugwu <ugwu.paul.n@gmail.com>
2023-12-14 16:29:22 +0100

    [Fix] Cleanup coomented code

--- hr_cbt_portal_recruitment/models/hr_applicant_documentation.py
+++ hr_cbt_portal_recruitment/models/hr_applicant_documentation.py
@@ -4,12 +4,6 @@ from odoo import models, fields, api, _
 class HrApplicantDocuments(models.Model):
     _name = 'hr.applicant.documentation'
 
-    # document_type = fields.Selection([
-    #     ('degree_cert', 'Degree Certificate'),
-    #     ('nysc_cert', 'NYSC Certificate'),
-    #     ('high_school_cert', 'High School Certificate'),
-    #     ('birth_cert', 'Birth Certificate'),
-    # ], string='Document Type', required=True)
     document_type = fields.Many2one('documentation.type')
     document_file = fields.Binary(string='Document File', attachment=True)
     filename = fields.Char("Filename")
