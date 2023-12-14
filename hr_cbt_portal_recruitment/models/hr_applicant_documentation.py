5d313a166b975a280d3ab6e598f8e80de4321f99
Paul Ugwu <ugwu.paul.n@gmail.com>
2023-12-14 16:13:57 +0100

    [Imp] Added data record for checklist

--- hr_cbt_portal_recruitment/models/hr_applicant_documentation.py
+++ hr_cbt_portal_recruitment/models/hr_applicant_documentation.py
@@ -4,11 +4,19 @@ from odoo import models, fields, api, _
 class HrApplicantDocuments(models.Model):
     _name = 'hr.applicant.documentation'
 
-    document_type = fields.Selection([
-        ('degree_cert', 'Degree Certificate'),
-        ('nysc_cert', 'NYSC Certificate'),
-        ('high_school_cert', 'High School Certificate'),
-        ('birth_cert', 'Birth Certificate'),
-    ], string='Document Type', required=True)
+    # document_type = fields.Selection([
+    #     ('degree_cert', 'Degree Certificate'),
+    #     ('nysc_cert', 'NYSC Certificate'),
+    #     ('high_school_cert', 'High School Certificate'),
+    #     ('birth_cert', 'Birth Certificate'),
+    # ], string='Document Type', required=True)
+    document_type = fields.Many2one('documentation.type')
     document_file = fields.Binary(string='Document File', attachment=True)
+    filename = fields.Char("Filename")
     applicant_id = fields.Many2one('hr.applicant', string='Applicant')
+
+
+class DocumentationType(models.Model):
+    _name = 'documentation.type'
+
+    name = fields.Char()
