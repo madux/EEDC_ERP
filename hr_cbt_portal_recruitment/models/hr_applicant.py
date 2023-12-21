18c5d4eb5843cc318496414cf62779427c5b4eeb
Paul Ugwu <ugwu.paul.n@gmail.com>
2023-12-21 13:06:30 +0100

    [Imp] Added audit reverse verification button

--- hr_cbt_portal_recruitment/models/hr_applicant.py
+++ hr_cbt_portal_recruitment/models/hr_applicant.py
@@ -151,14 +151,13 @@ class Applicant(models.Model):
                 rec.is_panelist_added =False 
 
     applicant_documentation_checklist = fields.One2many('hr.applicant.documentation', 'applicant_id', string='Checklists') 
-
     audited = fields.Boolean(defalt=False, string='Audited', readonly=True) # Boolean field to check whether someone has been auduted
-
 
     def action_audit_certify(self):
         if self.stage_id and self.stage_id.group_ids:
             user_group_ids = self.env.user.groups_id.ids
             group_ids_to_check = self.stage_id.group_ids.ids
+            # raise ValidationError(f'{[user_group_ids]} and {[group_ids_to_check]}')
 
             # Retrieve the names of the groups
             group_names_to_check = self.env['res.groups'].sudo().search([('id', 'in', group_ids_to_check)]).mapped('name')
@@ -166,9 +165,12 @@ class Applicant(models.Model):
             if not set(user_group_ids).intersection(group_ids_to_check):
                 raise ValidationError(f"You have to be in one of the groups with names {group_names_to_check} to approve")
 
-            self.audited = True
+            if self.audited == False:
+                self.audited = True
+            elif self.audited == True:
+                self.audited = False
         else:
-            self.audited = False
+            raise ValidationError(f"Set the approval groups")
 
 
 
