746388ac9dc6f0522b8f0e142c1e4bcc11067da0
madux3 <kingstongerald@gmail.com>
2024-01-09 11:14:00 +0100

    [FIX] - Fix employee base

--- eedc_addons/model/hr_employee.py
+++ eedc_addons/model/hr_employee.py
@@ -4,24 +4,27 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class HREmployee(models.Model):
-    _inherit = "hr.employee"
+# class HREmployee(models.Model):
+#     _inherit = "hr.employee"
 
-    house_address = fields.Char(string='House Address')
-    age = fields.Char(string='Age')
-    local_government = fields.Many2one('res.lga', string='LGA')
-    state_id = fields.Many2one('res.country.state', string='State')
-    state_of_origin = fields.Char(string='State of Origin')
-    lga = fields.Char(string='Local Goovernment') 
-    rank_id = fields.Many2one('hr.rank', string='Rank')
-    is_external_staff = fields.Boolean(string='Is External')
-    external_company_id = fields.Many2one('res.partner', string='External Company')
-    next_of_kin_ids = fields.Many2many('res.partner', 'nok_partner_rel', 'nok_partner_id', string='Next of Kin(s)')
+class HrEmployeeBase(models.AbstractModel):
+    _inherit = "hr.employee.base"
 
-    spouse_name = fields.Char(string='Spouse Name')
-    spouse_telephone = fields.Char(string='Spouse Telephone')
-    father_name = fields.Char(string="Father's Name")
-    father_phone = fields.Char(string="Father's Phone")
-    mother_name = fields.Char(string="Mother's Name")
-    mother_phone = fields.Char(string="Mother's Phone")
-    manager = fields.Boolean(string="Is a Manager")
+    house_address = fields.Char(string='House Address', groups="hr.group_hr_user")
+    age = fields.Char(string='Age', groups="hr.group_hr_user")
+    local_government = fields.Many2one('res.lga', string='LGA', groups="hr.group_hr_user")
+    state_id = fields.Many2one('res.country.state', string='State', groups="hr.group_hr_user")
+    state_of_origin = fields.Char(string='State of Origin', groups="hr.group_hr_user")
+    lga = fields.Char(string='Local Government', groups="hr.group_hr_user") 
+    rank_id = fields.Many2one('hr.rank', string='Rank', groups="hr.group_hr_user")
+    is_external_staff = fields.Boolean(string='Is External', groups="hr.group_hr_user")
+    external_company_id = fields.Many2one('res.partner', string='External Company', groups="hr.group_hr_user")
+    next_of_kin_ids = fields.Many2many('res.partner', 'nok_partner_rel', 'nok_partner_id', string='Next of Kin(s)', groups="hr.group_hr_user")
+
+    spouse_name = fields.Char(string='Spouse Name', groups="hr.group_hr_user")
+    spouse_telephone = fields.Char(string='Spouse Telephone', groups="hr.group_hr_user")
+    father_name = fields.Char(string="Father's Name", groups="hr.group_hr_user")
+    father_phone = fields.Char(string="Father's Phone", groups="hr.group_hr_user")
+    mother_name = fields.Char(string="Mother's Name", groups="hr.group_hr_user")
+    mother_phone = fields.Char(string="Mother's Phone", groups="hr.group_hr_user")
+    manager = fields.Boolean(string="Is a Manager", groups="hr.group_hr_user")
