eaf0f1646cd75199ca2a9f6ceb2c584781030f59
Paul Ugwu <ugwu.paul.n@gmail.com>
2024-02-27 14:53:16 +0100

    [Imp] Recruitment checklist send template

--- hr_cbt_portal_recruitment/models/hr_applicant_documentation.py
+++ hr_cbt_portal_recruitment/models/hr_applicant_documentation.py
@@ -5,8 +5,11 @@ class HrApplicantDocuments(models.Model):
     _name = 'hr.applicant.documentation'
 
     document_type = fields.Many2one('documentation.type')
-    document_file = fields.Binary(string='Document File', attachment=True)
-    filename = fields.Char("Filename")
+    document_file = fields.Many2one('ir.attachment', string='Document File')
+    # filename = fields.Char("Filename")
+    select = fields.Boolean("Confirmed", default=False)
+    applicant_submitted_document_file = fields.Many2one('ir.attachment', string='Applicant Document', attachment=True)
+    # applicant_filename = fields.Char("Applicant Filename")
     applicant_id = fields.Many2one('hr.applicant', string='Applicant')
 
 
@@ -14,3 +17,4 @@ class DocumentationType(models.Model):
     _name = 'documentation.type'
 
     name = fields.Char()
+    document_file = fields.Many2one('ir.attachment', string='Document File')
