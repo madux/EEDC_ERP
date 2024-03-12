999d872e325eefed70929b82373f910fd38c83d7
madux3 <kingstongerald@gmail.com>
2024-03-12 10:42:09 +0100

    [IMP] Added origin main features

--- hr_cbt_portal_recruitment/wizard/hr_confirmation_wizard.py
+++ hr_cbt_portal_recruitment/wizard/hr_confirmation_wizard.py
@@ -10,7 +10,6 @@ class HRConfirmWizard(models.TransientModel):
     hr_comment = fields.Text(string = 'Add Comment')
     hr_applicant_doc_id = fields.Many2one('hr.applicant.documentation')
 
-
     def confirm(self):
         if self.hr_applicant_doc_id:
             self.hr_applicant_doc_id.confirm_wizard_action(self.hr_comment)
