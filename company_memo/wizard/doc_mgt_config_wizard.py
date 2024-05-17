4f70ebf6f646b15e851ffe5dbff9257ffa712eb9
Paul <ugwu.paul.n@gmail.com>
2024-05-17 11:42:11 +0100

    [Fix] fixed the error in the inverse method _inverse_memo_type_id by dotting id in memo_type_id

--- company_memo/wizard/doc_mgt_config_wizard.py
+++ company_memo/wizard/doc_mgt_config_wizard.py
@@ -39,7 +39,7 @@ class DocMgtConfigWizard(models.TransientModel):
             if rec:
                 self.doc_mgt_config_id = rec
             else:
-                self.doc_mgt_config_id = self.env['doc.mgt.config'].create({'memo_type_id': self.memo_type_id}).id
+                self.doc_mgt_config_id = self.env['doc.mgt.config'].create({'memo_type_id': self.memo_type_id.id}).id
 
     
     @api.onchange('memo_type_id')
@@ -52,13 +52,13 @@ class DocMgtConfigWizard(models.TransientModel):
     def close_memo_config_wizard(self):
         if self.memo_type_id:
             if not self.doc_mgt_config_id:
-                rec = self.env['doc.mgt.config'].search([], limit=1).write({'memo_type_id': self.memo_type_id})
+                rec = self.env['doc.mgt.config'].search([], limit=1).write({'memo_type_id': self.memo_type_id.id})
                 if not rec:
-                    rec = self.env['doc.mgt.config'].create({'memo_type_id': self.memo_type_id})
+                    rec = self.env['doc.mgt.config'].create({'memo_type_id': self.memo_type_id.id})
                 self.doc_mgt_config_id = rec
                 return
             elif self.doc_mgt_config_id and (self.memo_type_id != self.doc_mgt_config_id.memo_type_id):
-                 self.env['doc.mgt.config'].write({'memo_type_id': self.memo_type_id})
+                 self.env['doc.mgt.config'].write({'memo_type_id': self.memo_type_id.id})
                  return
             else:
                 return
