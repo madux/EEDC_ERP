c2b8229e8a3aff629fbed5dc18c7d48f1c5f8608
madux3 <kingstongerald@gmail.com>
2024-05-28 14:45:06 +0100

    [FIX]

--- hr_cbt_portal_recruitment/controllers/main.py
+++ hr_cbt_portal_recruitment/controllers/main.py
@@ -202,7 +202,7 @@ class WebsiteHrRecruitment(http.Controller):
 				"specifylevel_qualification": post.get("specifylevel_qualification",False),
 				"image_1920": post.get("passport_img"),# base64.b64encode(post.get("passport_img").read()),
 			}
-			online_stage = self.env.ref('hr_cbt_portal_recruitment.hr_recruitment_stage_online_application')
+			online_stage = request.env.ref('hr_cbt_portal_recruitment.hr_recruitment_stage_online_application')
 			if online_stage:
 				vals.update({'stage_id': online_stage.id})
 			applicant = request.env['hr.applicant'].sudo().create(vals)
