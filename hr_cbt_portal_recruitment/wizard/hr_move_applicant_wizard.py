8b9a5ca5e97c87d7fd016248ea5d0602b6981103
madux3 <kingstongerald@gmail.com>
2024-05-28 16:16:54 +0100

    [FIX]

--- hr_cbt_portal_recruitment/wizard/hr_move_applicant_wizard.py
+++ hr_cbt_portal_recruitment/wizard/hr_move_applicant_wizard.py
@@ -76,7 +76,7 @@ class HrApplicantMove(models.TransientModel):
 				for pnl in uncompleted_score_sheets:
 					pnl_uncompleted = pnl.mapped('survey_user_input_ids').filtered(lambda st: st.state != 'done')
 					if pnl_uncompleted:
-						raise ValidationError(f"Applicant with name {applicant.name} at line {count} Score-Sheet has not been completely signed by the following panelists; {pnl.panelist_id.name}")
+						raise ValidationError(f"Line {count}: Applicant with name {applicant.partner_name}'s Score-Sheet has not been completely signed by the following panelists; {pnl.panelist_id.name}")
 
 	def action_move_applicant(self):
 		"""moves applicants to selected stage"""
