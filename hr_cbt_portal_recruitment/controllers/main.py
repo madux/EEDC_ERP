34f6a56ee01971d4d6303d68bb08cb1ea6809416
Paul <ugwu.paul.n@gmail.com>
2024-08-29 12:03:42 +0100

    [Fix] Removed comments

--- hr_cbt_portal_recruitment/controllers/main.py
+++ hr_cbt_portal_recruitment/controllers/main.py
@@ -28,9 +28,6 @@ class SurveyInherit(Survey):
             survey_sudo, answer_sudo = self._fetch_from_access_token(survey_token, answer_token)
             
             now = datetime.now()
-            # _logger.info(f'Current time: {now}')
-            # _logger.info(f'Survey start time: {survey_sudo.start_time}')
-            # _logger.info(f'Survey deadline: {survey_sudo.deadline}')
 
             if survey_sudo.start_time and datetime.now() < survey_sudo.start_time:
                 return 'survey_closed'
