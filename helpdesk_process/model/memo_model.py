796ba8eb78e7b8740edd5e2153c280489224b2f8
madux3 <kingstongerald@gmail.com>
2025-03-20 14:16:26 +0100

    [FIX] Added data configuration and setting up the views

--- helpdesk_process/model/memo_model.py
+++ helpdesk_process/model/memo_model.py
@@ -14,15 +14,20 @@ class ComplaintResolution(models.Model):
         default=lambda self: self.env.uid
         )
     memo_id = fields.Many2one(
-        'memo.model', 
+        'helpdesk.memo.model', 
         string='Memo ID'
         )
   
     
 class HelpdeskMemoModel(models.Model):
-    _inherit = 'memo.model'
+    _name = 'helpdesk.memo.model'
+    _inherit = ['memo.model']
 
     # attrs="{'required': [('memo_type_key', '=', 'helpdesk')]}"
+    helpdesk_memo_config_id = fields.Many2one(
+        'memo.config', 
+        string="Category", 
+        )
     customer_partner_id = fields.Many2one('res.partner', string='Customer ID')
     customer_phone = fields.Char('Customer phone')
     customer_phone2 = fields.Char('Customer phone 2')
@@ -43,10 +48,85 @@ class HelpdeskMemoModel(models.Model):
     complaint_close_date = fields.Date('Closed Date')
     complaint_duration = fields.Integer('Duration')
     complaint_description = fields.Text('Complaints')
-    complaint_resolution_ids = fields.Many2many(
+    complaint_resolution_ids = fields.One2many(
         'complaint.resolution',
+        'memo_id',
         string='Resolution line'
         )
+    
+    user_owned_cash_advance_ids = fields.Many2many(
+        'memo.model', 
+        'user_owned_cash_helpdesk_advance_rel',
+        'user_owned_cash_helpdesk_advance_id',
+        'memo_id', string="User owned cash helpdesk advances", store=False)
+    
+    approver_ids = fields.Many2many(
+        'hr.employee',
+        'memo_model_helpdesk_employee_rel',
+        'memo_id',
+        'hr_employee_id',
+        string='Approvers'
+        )
+    
+    invoice_ids = fields.Many2many(
+        'hr.employee',
+        'memo_model_helpdesk_employee_rel',
+        'memo_id',
+        'hr_employee_id',
+        string='Approvers'
+        )
+    partner_ids = fields.Many2many(
+        'res.partner', 
+        'memo_res_helpdesk_partner_rel',
+        'memo_res_partner_id',
+        'memo_partner_id',
+        string='Reciepients', 
+        )
+    attachment_ids = fields.Many2many(
+        'ir.attachment', 
+        'memo_ir_attachment_helpdesk_rel',
+        'memo_ir_attachment_id',
+        'ir_attachment_memo_id',
+        string='Attachment', 
+        store=True,
+        domain="[('res_model', '=', 'memo.model')]"
+        )
+    memo_sub_stage_ids = fields.Many2many(
+        'memo.sub.stage', 
+        'memo_sub_stage_helpdesk_rel',
+        'memo_sub_stage_id',
+        'memo_id',
+        string='Sub Stages', 
+        store=True,
+        )
+    
+    @api.onchange('helpdesk_memo_config_id')
+    def get_helpdesk_memo_config_id_stage_id(self):
+        """ Gives default stage_id """
+        if self.helpdesk_memo_config_id:
+            if self.helpdesk_memo_config_id.stage_ids:
+                memo_setting_stage = self.helpdesk_memo_config_id.stage_ids[0]
+                self.stage_id = memo_setting_stage.id if memo_setting_stage else False
+                self.memo_setting_id = self.helpdesk_memo_config_id.id
+                self.memo_type_key = self.memo_type.memo_key 
+                self.requested_department_id = self.employee_id.department_id.id
+                self.users_followers = [
+                            (4, self.employee_id.administrative_supervisor_id.id),
+                            ] 
+            else:
+                self.memo_type = False
+                self.stage_id = False
+                self.memo_setting_id = False
+                self.memo_type_key = False
+                self.requested_department_id = False
+                msg = f"No stage configured for the select configuration. Please contact administrator"
+                return {'warning': {
+                            'title': "Validation",
+                            'message':msg,
+                        }
+                }
+        else:
+            self.stage_id = False
     
     def confirm_memo_helpdesk (self):
         for complain in self:
