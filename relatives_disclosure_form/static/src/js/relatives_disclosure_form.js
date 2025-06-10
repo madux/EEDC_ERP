8adcf33ac5a296161ecce9f56ba50f2c3bf89c2e
MichaelNdunwa <ndunwa240@gmail.com>
2025-06-10 17:09:03 +0100

    Improved the webview form

--- relatives_disclosure_form/static/src/js/relatives_disclosure_form.js
+++ relatives_disclosure_form/static/src/js/relatives_disclosure_form.js
@@ -1,12 +1,13 @@
-odoo.define('relatives_disclosure_form.maiden_toggle', function(require){
+odoo.define('relatives_disclosure_form.form_js', function (require) {
     "use strict";
     $(document).ready(function () {
+        // Maiden name show/hide logic
         function toggleMaidenName() {
             var gender = $('#gender').val();
             var marital = $('#marital_status').val();
             var maidenGroup = $('#maiden_name_group');
             var maidenInput = maidenGroup.find('input[name="maiden_name"]');
-            if (gender === 'male' && marital === 'married') {
+            if (gender === 'female' && marital === 'married') {
                 maidenGroup.show();
                 maidenInput.prop('required', true);
             } else {
@@ -17,15 +18,43 @@ odoo.define('relatives_disclosure_form.maiden_toggle', function(require){
         }
         $('#gender, #marital_status').on('change', toggleMaidenName);
         toggleMaidenName();
-        console.log("Michael 1")
 
-        // Custom validation on submit
-        $('.relatives-disclosure-form').on('submit', function(e){
+        // Add relative row logic
+        $('#add-relative').on('click', function () {
+            var $container = $('#relatives-container');
+            var $district = $('#template_district').clone().removeAttr('id').show().attr('name', 'relative_district[]').addClass('form-control d-inline-block w-auto mx-1');
+            var $department = $('#template_department').clone().removeAttr('id').show().attr('name', 'relative_department[]').addClass('form-control d-inline-block w-auto mx-1');
+            var $row = $(`
+                <div class="relative-row mb-2">
+                    <input type="text" name="relative_name[]" placeholder="Relative Name" required class="form-control d-inline-block w-auto mx-1"/>
+                    <select name="relationship[]" required class="form-control d-inline-block w-auto mx-1">
+                        <option value="">Relationship</option>
+                        <option value="spouse">Spouse</option>
+                        <option value="parent">Parent</option>
+                        <option value="sibling">Sibling</option>
+                        <option value="child">Child</option>
+                        <option value="niece">Niece</option>
+                        <option value="nephew">Nephew</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+            `);
+            $row.append($district);
+            $row.append($department);
+            $row.append('<button type="button" class="btn btn-danger btn-sm remove-relative mx-1">Remove</button>');
+            $container.append($row);
+            $row.find('.remove-relative').on('click', function () {
+                $row.remove();
+            });
+        });
+
+        // Custom validation
+        $('.relatives-disclosure-form').on('submit', function (e) {
             var errorMessages = [];
             var $form = $(this);
 
             // Check all visible required fields
-            $form.find('input, select, textarea').each(function(){
+            $form.find('input, select, textarea').each(function () {
                 var $field = $(this);
                 if ($field.is(':visible') && $field.prop('required') && !$field.val()) {
                     var label = $field.closest('.form-group').find('label').text() || $field.attr('name');
@@ -35,8 +64,6 @@ odoo.define('relatives_disclosure_form.maiden_toggle', function(require){
                     $field.removeClass('is-invalid');
                 }
             });
-
-            // You can also check for special rules (e.g., signature file only certain extensions, etc.)
 
             if (errorMessages.length > 0) {
                 e.preventDefault(); // Stop form from submitting
@@ -50,4 +77,3 @@ odoo.define('relatives_disclosure_form.maiden_toggle', function(require){
         });
     });
 });
-
