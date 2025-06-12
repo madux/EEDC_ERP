6a6066cfcd75a335a7d46a2f8393d4d17ad251a1
MichaelNdunwa <ndunwa240@gmail.com>
2025-06-12 07:32:09 +0100

    commented out select

--- relatives_disclosure_form/static/src/js/relatives_disclosure_form.js
+++ relatives_disclosure_form/static/src/js/relatives_disclosure_form.js
@@ -20,33 +20,33 @@ odoo.define('relatives_disclosure_form.form_js', function (require) {
         toggleMaidenName();
 
         // Add relative row logic
-        $('#add-relative').on('click', function () {
-            var $container = $('#relatives-container');
-            var $district = $('#template_district').clone().removeAttr('id').show().attr('name', 'relative_district[]').addClass('form-control d-inline-block w-auto mx-1');
-            var $department = $('#template_department').clone().removeAttr('id').show().attr('name', 'relative_department[]').addClass('form-control d-inline-block w-auto mx-1');
-            var $row = $(`
-                <div class="relative-row mb-2">
-                    <input type="text" name="relative_name[]" placeholder="Relative Name" required class="form-control d-inline-block w-auto mx-1"/>
-                    <select name="relationship[]" required class="form-control d-inline-block w-auto mx-1">
-                        <option value="">Relationship</option>
-                        <option value="spouse">Spouse</option>
-                        <option value="parent">Parent</option>
-                        <option value="sibling">Sibling</option>
-                        <option value="child">Child</option>
-                        <option value="niece">Niece</option>
-                        <option value="nephew">Nephew</option>
-                        <option value="other">Other</option>
-                    </select>
-                </div>
-            `);
-            $row.append($district);
-            $row.append($department);
-            $row.append('<button type="button" class="btn btn-danger btn-sm remove-relative mx-1">Remove</button>');
-            $container.append($row);
-            $row.find('.remove-relative').on('click', function () {
-                $row.remove();
-            });
-        });
+        // $('#add-relative').on('click', function () {
+        //     var $container = $('#relatives-container');
+        //     var $district = $('#template_district').clone().removeAttr('id').show().attr('name', 'relative_district[]').addClass('form-control d-inline-block w-auto mx-1');
+        //     var $department = $('#template_department').clone().removeAttr('id').show().attr('name', 'relative_department[]').addClass('form-control d-inline-block w-auto mx-1');
+        //     var $row = $(`
+        //         <div class="relative-row mb-2">
+        //             <input type="text" name="relative_name[]" placeholder="Relative Name" required class="form-control d-inline-block w-auto mx-1"/>
+        //             <select name="relationship[]" required class="form-control d-inline-block w-auto mx-1">
+        //                 <option value="">Relationship</option>
+        //                 <option value="spouse">Spouse</option>
+        //                 <option value="parent">Parent</option>
+        //                 <option value="sibling">Sibling</option>
+        //                 <option value="child">Child</option>
+        //                 <option value="niece">Niece</option>
+        //                 <option value="nephew">Nephew</option>
+        //                 <option value="other">Other</option>
+        //             </select>
+        //         </div>
+        //     `);
+        //     $row.append($district);
+        //     $row.append($department);
+        //     $row.append('<button type="button" class="btn btn-danger btn-sm remove-relative mx-1">Remove</button>');
+        //     $container.append($row);
+        //     $row.find('.remove-relative').on('click', function () {
+        //         $row.remove();
+        //     });
+        // });
 
         // Step navigation
         $('#next-to-relatives').on('click', function (e) {
