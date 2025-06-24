07f37f093247bbadc59c12cc6bc00c6ee41e51bf
Michael Ndunwa <ndunwa240@gmail.com>
2025-06-24 15:23:23 +0100

    relatives tab iimproved ui

--- relatives_disclosure_form/static/src/js/relatives_disclosure_form.js
+++ relatives_disclosure_form/static/src/js/relatives_disclosure_form.js
@@ -1,6 +1,8 @@
 odoo.define('relatives_disclosure_form.form_js', function (require) {
     "use strict";
+
     $(document).ready(function () {
+        console.log("Relatives Disclosure Form JS Loaded");
         // Maiden name show/hide logic
         function toggleMaidenName() {
             var gender = $('#gender').val();
@@ -19,30 +21,62 @@ odoo.define('relatives_disclosure_form.form_js', function (require) {
         $('#gender, #marital_status').on('change', toggleMaidenName);
         toggleMaidenName();
 
-        // Add relative row logic
-        $('#add-relative').on('click', function () {
+        // Ensure add-relative is bound once
+        $(document).off('click', '#add-relative').on('click', '#add-relative', function () {
             var $container = $('#relatives-container');
-            var $district = $('#template_district').clone().removeAttr('id').show().attr('name', 'relative_district[]').addClass('form-control d-inline-block w-auto mx-1');
-            var $department = $('#template_department').clone().removeAttr('id').show().attr('name', 'relative_department[]').addClass('form-control d-inline-block w-auto mx-1');
-            var $row = $(`
-                <div class="relative-row mb-2">
-                    <input type="text" name="relative_name[]" placeholder="Relative Name" required class="form-control d-inline-block w-auto mx-1"/>
-                    <select name="relationship[]" required class="form-control d-inline-block w-auto mx-1">
-                        <option value="">Relationship</option>
-                        <option value="spouse">Spouse</option>
-                        <option value="parent">Parent</option>
-                        <option value="sibling">Sibling</option>
-                        <option value="child">Child</option>
-                        <option value="niece">Niece</option>
-                        <option value="nephew">Nephew</option>
-                        <option value="other">Other</option>
-                    </select>
-                </div>
-            `);
-            $row.append($district);
-            $row.append($department);
-            $row.append('<button type="button" class="btn btn-danger btn-sm remove-relative mx-1">Remove</button>');
+
+            var $district = $('#template_district')
+                .clone(false)
+                .removeAttr('id')
+                .show()
+                .attr('name', 'relative_district[]')
+                .addClass('form-control');
+            $district.find('option:first').text('Select District');
+
+            var $department = $('#template_department')
+                .clone(false)
+                .removeAttr('id')
+                .show()
+                .attr('name', 'relative_department[]')
+                .addClass('form-control');
+            $department.find('option:first').text('Select Department');
+
+            var $row = $(
+                `<div class="relative-row border rounded p-3 mb-3">
+                    <div class="form-group">
+                        <label>Relative Name</label>
+                        <input type="text" name="relative_name[]" class="form-control" required placeholder="Enter Relative Name"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Relationship</label>
+                        <select name="relationship[]" class="form-control" required>
+                            <option value="">Select Relationship</option>
+                            <option value="spouse">Spouse</option>
+                            <option value="parent">Parent</option>
+                            <option value="sibling">Sibling</option>
+                            <option value="child">Child</option>
+                            <option value="niece">Niece</option>
+                            <option value="nephew">Nephew</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>District</label>
+                    </div>
+                    <div class="form-group">
+                        <label>Department</label>
+                    </div>
+                    <div class="text-right">
+                        <button type="button" class="btn btn-sm remove-relative" style="background-color:#dc3545; color:white;">Remove</button>
+                    </div>
+                </div>`
+            );
+
+            $row.find('.form-group').eq(2).append($district);
+            $row.find('.form-group').eq(3).append($department);
+
             $container.append($row);
+
             $row.find('.remove-relative').on('click', function () {
                 $row.remove();
             });
@@ -50,7 +84,6 @@ odoo.define('relatives_disclosure_form.form_js', function (require) {
 
         // Step navigation
         $('#next-to-relatives').on('click', function (e) {
-            // Validate required fields in the "details" tab first
             var valid = true;
             var errorMessages = [];
             $('#details').find('input, select, textarea').each(function () {
@@ -74,7 +107,6 @@ odoo.define('relatives_disclosure_form.form_js', function (require) {
                 $('#form-error-message').hide();
             }
 
-            // Move to relatives tab
             $('#details-tab').removeClass('active');
             $('#relatives-tab').addClass('active');
             $('#details').removeClass('show active');
@@ -82,23 +114,18 @@ odoo.define('relatives_disclosure_form.form_js', function (require) {
         });
 
         $('#prev-to-details').on('click', function (e) {
-            // Move back to details tab
             $('#relatives-tab').removeClass('active');
             $('#details-tab').addClass('active');
             $('#relatives').removeClass('show active');
             $('#details').addClass('show active');
         });
 
-        // Custom validation on submit (on relatives tab)
+        // Form validation on submit
         $('.relatives-disclosure-form').on('submit', function (e) {
             var errorMessages = [];
-            var $form = $(this);
-
-            // Only validate fields in the relatives tab for submission
             $('#relatives').find('input, select, textarea').each(function () {
                 var $field = $(this);
                 if ($field.prop('required') && !$field.val()) {
-                    var label = $field.closest('.relative-row').find('input[name="relative_name[]"]').val() || $field.attr('name');
                     errorMessages.push('Please complete all relative fields.');
                     $field.addClass('is-invalid');
                 } else {
@@ -107,7 +134,7 @@ odoo.define('relatives_disclosure_form.form_js', function (require) {
             });
 
             if (errorMessages.length > 0) {
-                e.preventDefault(); // Stop form from submitting
+                e.preventDefault();
                 $('#form-error-message').html(errorMessages.join('<br>')).show();
                 $('html, body').animate({
                     scrollTop: $('#form-error-message').offset().top - 100
