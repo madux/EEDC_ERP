c371281f4f82f04b99866dc6fe2b0fd377849a5e
Paul <ugwu.paul.n@gmail.com>
2025-07-02 09:44:43 +0100

    [Fix]

--- helpdesk_process/__manifest__.py
+++ helpdesk_process/__manifest__.py
@@ -7,8 +7,8 @@
     'depends': ['base', 'hr', 'company_memo',],#, 'hr_payroll_addons'],
     'description': "ODOO Memo helpdesk: helps to management processes involved in helpesk",
     "data": [
-        'security/ir.model.access.csv',
         'security/helpdesk_groups.xml',
+        'security/ir.model.access.csv',
         'views/memo_view_helpdesk.xml',
         'views/memo_view_helpdesk_kaban.xml',
         'data/memo_type.xml',
