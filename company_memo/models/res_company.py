a1a0d4061af50bd24d6e546a561ba2176b2a8655
madux3 <kingstongerald@gmail.com>
2025-07-25 11:15:52 +0100

    [Fix] main hotfix

--- company_memo/models/res_company.py
+++ company_memo/models/res_company.py
@@ -7,8 +7,8 @@ from odoo.exceptions import ValidationError
 class resCompany(models.Model):
     _inherit = 'res.company'
 
-    # user_signature = fields.Binary("Upload User Signature")
-    # account_default_debit_account_id = fields.Many2one(
-    #     "account.account", 
-    #     string="Default Account",
-    #     )
+    user_signature = fields.Binary("Upload User Signature")
+    account_default_debit_account_id = fields.Many2one(
+        "account.account", 
+        string="Default Account",
+        )
