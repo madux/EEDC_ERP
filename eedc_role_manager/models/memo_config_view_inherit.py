cba0f5945c99a5cd5c0e1dbbf4f7ffdda83978bb
Paul Ugwu <ugwu.paul.n@gmail.com>
2025-10-01 04:29:21 +0100

    [Imp] Window action to synchronise approvers in memo config

--- eedc_role_manager/models/memo_config_view_inherit.py
+++ eedc_role_manager/models/memo_config_view_inherit.py
@@ -1,5 +1,72 @@
-from odoo import models, fields
+from odoo import models, fields,_, SUPERUSER_ID
 from odoo.exceptions import ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class MemoConfig(models.Model):
+    _inherit = 'memo.config'
+    
+    def action_sync_approvers(self):
+        """
+        Synchronize all approvers for the selected memo configurations.
+        This will trigger a full resync of all users who have roles mentioned
+        in the approval_role_ids of the stages belonging to these configs.
+        """
+        config_ids = self.env.context.get('active_ids', [])
+        configs = self.browse(config_ids)
+        
+        stages = self.env['memo.stage'].search([('memo_config_id', 'in', configs.ids)])
+        
+        roles = stages.mapped('approval_role_ids')
+        
+        if not roles:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': _('Warning'),
+                    'message': _('No approval roles found in the selected configurations.'),
+                    'type': 'warning',
+                    'sticky': False,
+                }
+            }
+        
+        users = roles.mapped('user_ids').filtered(lambda u: u.id != SUPERUSER_ID)
+        
+        if not users:
+            _logger.info("action_sync_approvers: no users found with approval roles for configs %s", configs.mapped('name'))
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': _('Warning'),
+                    'message': _('No users assigned to the approval roles in these configurations.'),
+                    'type': 'warning',
+                    'sticky': False,
+                }
+            }
+        
+        _logger.info("action_sync_approvers: syncing %d users for %d configs", len(users), len(configs))
+        
+        for user in users:
+            try:
+                user.sudo()._sync_approvals_from_roles()
+                _logger.info("action_sync_approvers: synced user %s (id=%s)", user.login, user.id)
+            except Exception:
+                _logger.exception("action_sync_approvers: failed to sync user %s (id=%s)", user.login, user.id)
+        
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': _('Success'),
+                'message': _('Approvers synchronized successfully for %d user(s).') % len(users),
+                'type': 'success',
+                'sticky': False,
+            }
+        }
+    
 
 class MemoStageInherit(models.Model):
     _inherit = 'memo.stage'
