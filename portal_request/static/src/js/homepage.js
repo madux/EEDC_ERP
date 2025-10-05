4bb177f99463bb6f25a0646d8b733ea98657b273
madux3 <kingstongerald@gmail.com>
2025-10-06 00:44:04 +0100

    [FIX]

--- portal_request/static/src/js/homepage.js
+++ portal_request/static/src/js/homepage.js
@@ -46,7 +46,8 @@ odoo.define('portal_request.portal_homepage', function (require) {
                 $('#switchModeBtn1').attr('color', 'white');
 
             }
-        };
+        }
+
         $('#switchModeBtn').click(function () {
             const savedMode = localStorage.getItem('mode');
             setMode(savedMode === 'light');
@@ -54,5 +55,96 @@ odoo.define('portal_request.portal_homepage', function (require) {
             setMode(isLight);
             console.log('Clicked dark mode', localStorage.getItem('mode'))
         });
-    })
+
+        $('#password_reset_successful_alert').click(function () {
+            $('#password_reset_successful_alert').hide()
+        });
+
+        
+
+        $('#reset_password_button').click(function(ev){
+            let targetElement = $(ev.target).attr('id');
+            let $btn = $('#reset_password_button');
+            let $btnHtml = $btn.html()
+            let email = $('#employee_email')
+            let staff_number = $('#employee_staff_id')
+            $btn.attr('disabled', 'disabled');
+            $btn.prepend('<i class="fa fa-spinner fa-spin"/> ');
+            $.blockUI({
+                'message': '<h2 class="card-name">Resetting password ...</h2>'
+            });
+            const Data = {
+                'employee_email': email.val(),
+                'staff_number': staff_number.val(),
+            }
+            console.log('reseting password')
+            $.ajax({
+                type: "POST",
+                url: "/reset/password",
+                data: JSON.stringify(Data),
+                contentType: "application/json",  
+                dataType: "json",               
+                processData: false,
+                success: function (data) {
+                    $btn.attr('disabled', false);
+                    $btn.html($btnHtml)
+                    $.unblockUI()
+                    if(data.status){
+                        console.log('updating employee password reset => '+ JSON.stringify(data))
+                        email.val('');
+                        staff_number.val('');
+                        email.attr('required', true);
+                        staff_number.attr('required', true);
+                        $('#reset_password_dialog').hide()
+                        $('#password_reset_successful_alert').show()
+                        $('#reset_message').text(data.message);
+                        // window.location.href = `/`
+                        // alert(data.message);
+                    }else{
+                        alert(data.message);
+                    }
+                },
+                error: function (xhr) {
+                    $btn.attr('disabled', false);
+                    $btn.html($btnHtml)
+                    $.unblockUI()
+                    alert(xhr.responseText);
+                }
+            });
+            // $.ajax({
+            //     type: "POST",
+            //     enctype: 'multipart/form-data',
+            //     url: "/reset/password",
+            //     data: JSON.stringify(data),
+            //     processData: false,
+            //     contentType: false,
+            //     cache: false,
+            //     timeout: 800000,
+            // }).then(function (data) {
+            //     $btn.attr('disabled', false);
+            //     $btn.html($btnHtml)
+            //     $.unblockUI()
+            //     if(data.status){
+            //         console.log('updating employee password reset => '+ JSON.stringify(data))
+            //         email.val('');
+            //         staff_number.val('');
+            //         email.attr('required', true);
+            //         staff_number.attr('required', true);
+            //         $('#reset_password_dialog').hide()
+            //         $('#password_reset_successful_alert').show()
+            //         window.location.href = `/`
+            //         // alert(data.message);
+            //     }else{
+            //         alert(data.message);
+            //     }
+                
+            // }).catch((error) => {
+            //     $btn.attr('disabled', false);
+            //     $btn.html($btnHtml)
+            //     $.unblockUI()
+            //     alert(error)
+            // });
+             
+        });
+    });
 });
